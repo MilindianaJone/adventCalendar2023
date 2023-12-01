07e04c45a0b7692d95f90c7b007b3db9648e307d
MilindianaJone <mbordia8@gmail.com>
2023-12-01 21:32:57 +1000

    Day 1 complete

---
+++ day1/trebuchet.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+"""
+Detects the calibration value of a single line
+"""
+def lineCalibration(line: str) -> int:
+    starting = 0
+    ending = 0
+    startingFound = False
+    for char in line:
+        if char >= '1' and char <= '9':
+            if not startingFound:
+                starting = ord(char) - 48
+                startingFound = True
+            ending = ord(char) - 48
+    calibration = starting*10 + ending
+    return calibration
+
+"""
+Main function. Loops through each line and returns sum
+"""
+def main(input: str) -> int:
+    calibrationSum = 0
+    textMatrix = input.split('\n')
+    print(textMatrix)
+    for line in textMatrix:
+        calibrationSum += lineCalibration(line)
+    print(calibrationSum)
+    return calibrationSum
+
+if __name__ == "__main__":
+    with open('trebuchet.txt', 'r') as file:
+        data = file.read()
+    #txt = Path('trebuchet.txt').read_text()
+    main(data)
