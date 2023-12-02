ee9db01f41ac9c87d5098e1de07974113f476929
MilindianaJone <mbordia8@gmail.com>
2023-12-02 15:09:04 +1000

    Added basic functionality for cube conundrum problem

---
+++ day2/cube_conundrum.py
@@ -0,0 +1,26 @@
+"""
+Checks if game is valid. Returns true if valid, false if not.
+"""
+def gameCheck(line: str) -> bool:
+    return True
+
+"""
+Main function. Loops through each line and returns sum
+"""
+def main(input: str) -> int:
+    gameSum = 0
+    textMatrix = input.split('\n')
+    print(textMatrix)
+    for index, line in enumerate(textMatrix):
+        if gameCheck(line):
+            gameSum += index + 1
+    print(gameSum)
+    return gameSum
+
+if __name__ == "__main__":
+    with open('cube_conunundrum_input.txt', 'r') as file:
+        data = file.read()
+    main(data)
+
+
+
