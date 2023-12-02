e67534b174dbeb367a44e8cdc6f147fdcd93a0d7
MilindianaJone <mbordia8@gmail.com>
2023-12-02 15:18:40 +1000

    Checks if rules are violated for cube conundrum

--- day2/cube_conundrum.py
+++ day2/cube_conundrum.py
@@ -2,6 +2,27 @@
 Checks if game is valid. Returns true if valid, false if not.
 """
 def gameCheck(line: str) -> bool:
+    # Isolate section of line to check
+    line = line.split(":")[1]
+
+    # Split a game into its sets
+    gameSets = line.split(";")
+
+    # Within each set, check if rules are violated. If so, return False
+    for sets in gameSets:
+        redIndex = sets.find("red")
+        greenIndex = sets.find("green")
+        blueIndex = sets.find("blue")
+
+        if redIndex != -1:
+            if sets[redIndex - 2] > 12:
+                return False
+        if greenIndex != -1:
+            if sets[greenIndex - 2] > 13:
+                return False
+        if blueIndex != -1:
+            if sets[blueIndex - 2] > 14:
+                return False
     return True
 
 """
@@ -17,7 +38,7 @@ def main(input: str) -> int:
     return gameSum
 
 if __name__ == "__main__":
-    with open('cube_conunundrum_input.txt', 'r') as file:
+    with open('cube_conundrum_input.txt', 'r') as file:
         data = file.read()
     main(data)
 
